5b2672fc43a65c746ff3d5fcdb00992656e4d704
Nafees Munsarim <nafees.munsarim@gmail.com>
2026-05-04 22:37:01 +0600

    Fibonacci

---
+++ Easy/Fibonacci.cpp
@@ -0,0 +1,36 @@
+// Question :
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastRead ios_base::sync_with_stdio(0);cin.tie(0);
+#define ULL unsigned long long
+#define LL long long
+#define eps 1e-9
+#define inf 0x3f3f3f3f
+#define INF 2e18
+#define all(a) a.begin(),a.end()
+#define Unique(a) sort(all(a)),a.erase(unique(all(a)),a.end())
+
+int main() {
+    FastRead;
+
+int n;
+if (cin >> n) {
+        if (n == 1 || n == 2) {
+            cout << 1 << endl;
+            return 0;
+        }
+    // Used long long to prevent integer overflow since F(49) > 2.14 billion
+        long long a = 1;
+        long long b = 1;
+        long long next_fib;
+
+        for (int i = 3; i <= n; i++) {
+            next_fib = a + b;
+            a = b;
+            b = next_fib;
+        }
+
+        cout << b << endl;
+    }
+}
